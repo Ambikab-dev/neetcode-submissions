f0b979d4d279bde9cd5f0b2f8457364b8d095028
Ambikab-dev <ambikamanirao984@gmail.com>
2026-08-30 20:29:23 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            if(Character.isLetter(c) || Character.isDigit(c)){
+                sb.append(Character.toLowerCase(c));
+            }
+        }
+        String temp = sb.toString();
+        int left = 0, right = temp.length()-1;
+        while(left <= right){
+            if(temp.charAt(left) != temp.charAt(right) ){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
